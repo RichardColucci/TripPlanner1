9d978e102159d809136941b7ae9f444d68979834
Richard Colucci <rcolucci09@gmail.com>
2019-01-23 16:15:07 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('Welcome to Trip Planner');
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoicmNvbHVjY2kwOSIsImEiOiJjanI5b2Z3MGQwamtlNDRwY245ZW9rd3U4In0.pkSrGVT-gjMmH2r63nuT3A';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
